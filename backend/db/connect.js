0b1a61938a7f388bc105968c69558d9d9e7144ba
Sam Robert <91603698+bobTheBuilderCodes@users.noreply.github.com>
2024-03-11 17:55:14 +0000

    DB Connection done

---
+++ backend/db/connect.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+
+function connectToDB(url){
+    return mongoose.connect(url)
+}
+
+module.exports = connectToDB
+
+
+
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,35 @@
-const express = require("express")
 
-const app = express()
+const express = require("express");
+const connectToDB = require("../db/connect");
 
-app.get("/" ,(req, res)=>{
-res.send("Hello WORLD.")
-})
+require("dotenv").config()
 
-const PORT = process.env.PORT || 5000
+const app = express();
 
-app.listen(PORT , ()=>{
-    console.log(`Server listening on port ${PORT}`)
-})
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("Hello, Worlddd");
+});
+
+
+async function startServer(){
+    try {
+        console.log("Connecting to DB....")
+        await connectToDB(process.env.MONGO_URI)
+        app.listen(PORT, console.log(`Server connected at port ${PORT}`))
+        console.log(`Connection to DB successful...`)
+        
+    } catch (error) {
+        console.log(error)
+
+    }
+}
+
+startServer()
+
+// app.listen(PORT, console.log(`Server connected at port ${PORT}`))
+
+
